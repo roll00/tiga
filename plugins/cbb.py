00a0d157a60828f714701c033b6a59f43c030eef
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-02 23:06:50 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,7 +5,7 @@
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot import Bot
-from config import CHANNEL, GROUP, OWNER
+from config import CHANNEL1, CHANNEL2, GROUP, OWNER
 
 
 @Bot.on_callback_query()
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>ᴀʙᴏᴜᴛ ᴛʜɪs ʙᴏᴛ​:\n\n👑 ᴏᴡɴᴇʀ​: <a href='https://t.me/{OWNER}'>Owner</a>\n📣 ᴄʜᴀɴɴᴇʟ​: <a href='https://t.me/{CHANNEL}'>Channel</a>\n👥 ɢʀᴏᴜᴘ​: <href='https://t.me/{GROUP}'>Grup</a></b>\n",
+            text=f"<b>ᴀʙᴏᴜᴛ ᴛʜɪs ʙᴏᴛ​:\n\n👑 ᴏᴡɴᴇʀ​: <a href='https://t.me/{OWNER}'>Owner</a>\n📣 ᴄʜᴀɴɴᴇʟ¹​: <a href='https://t.me/{CHANNEL1}'>Channel¹</a>\n📣 ᴄʜᴀɴɴᴇʟ²: <a href='https://t.me/{CHANNEL2}'>Channel²</a>\n👥 ɢʀᴏᴜᴘ​: <href='https://t.me/{GROUP}'>Grup</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]]
